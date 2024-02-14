a14d4d8cde504da4729b9d5a2940c0d5c4ea5875
GFX23 <Spacil-Tomas@seznam.cz>
2024-02-14 18:23:15 +0100

    test page edit

--- app/(pages)/test/page.tsx
+++ app/(pages)/test/page.tsx
@@ -4,23 +4,23 @@ import { NextPage } from "next";
 import React from "react";
 
 import { Input } from "@/app/_components/Input";
-import { fillDB } from "@/db/fill/fill";
+// import { fillDB } from "@/db/fill/fill";
 
 const TestPage: NextPage = () => {
-	const handleClick = async () => {
-		const insert = await fillDB();
-		console.debug(insert);
-	};
+	// const handleClick = async () => {
+	// 	const insert = await fillDB();
+	// 	console.debug(insert);
+	// };
+	const testLabel = "Testovací stránka";
 
 	return (
 		<div>
-			<h1>TestPage Page</h1>
+			<h1>{testLabel}</h1>
 			<p>Manage your TestPage on this page.</p>
 			<Input label="Zadej hodiny" />
-
-			<button style={{ color: "white" }} type="button" onClick={handleClick}>
+			{/* <button style={{ color: "white" }} type="button" onClick={handleClick}>
 				Fill DB
-			</button>
+			</button> */}
 		</div>
 	);
 };
